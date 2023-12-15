88f918c5dae3ecfa548a03141fbce8ed346d7e7f
Farhana <farhanafarhana043@gmail.com>
2023-12-14 20:42:48 -0500

    object and object instance creating and using

--- oopInPython/secondClass.py
+++ oopInPython/secondClass.py
@@ -15,16 +15,40 @@ class Student:
 
 
 class Course:
-  def __init__(self,name,max_student):
+  def __init__(self,name,max_students):
     self.name = name
-    self.max_student = max_student
+    self.max_students = max_students
     self.students = []  # we are creating  a empty list to add student in Course 
 
 
+   #function to add student to course
+
+  def add_student(self,student):
+    if len(self.students) < self.max_students:
+      self.students.append(student)
+      return True
+    return False
+  
+
+  def get_average_grade(self):
+    value = 0
+    for student in self.students:
+      value += student.get_grade()
+
+    return value/len(self.students)  
 
 
+s1 = Student("Tim" ,19, 95)
+s2 = Student("Bill",19,75)
+s3 = Student("Jimmy",19,98)
 
+course = Course("Science",2)
+course.add_student(s1)
+course.add_student(s2)
 
+#print(course.students[0].name)
+
+print(course.get_average_grade())
 
 
 
